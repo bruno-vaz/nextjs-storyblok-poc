46415414986938d7027f52afd73d5f61b25d1201
bruno-vaz <hello@brunovaz.dev>
2022-05-26 18:27:04 -0300

    2 files modified

--- components/Grid.js
+++ components/Grid.js
@@ -1,7 +1,7 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const Grid = ({ blok }) => (
-  <ul className="grid grid-cols-3 py-8 mb-6" {...storyblokEditable(blok)} key={blok._uid}>
+  <ul className="grid grid-cols-3 gap-8 py-8 mb-6" {...storyblokEditable(blok)} key={blok._uid}>
     {blok.columns.map((blok) => (
       <li key={blok._uid}>
         <StoryblokComponent blok={blok} />
