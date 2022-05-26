a79c47f13a01b49d8f0058917e263d1985aca157
Bruno Vaz <hello@brunovaz.dev>
2022-05-26 18:10:05 -0300

    Add text field support to Feature blok

--- components/Feature.js
+++ components/Feature.js
@@ -3,7 +3,10 @@ import { storyblokEditable } from '@storyblok/react';
 const Feature = ({ blok }) => {
   return (
     <div className="py-2" {...storyblokEditable(blok)} key={blok._uid}>
-      <h2 className="text-lg"> {blok.name} </h2>
+      <h2 className="text-lg font-bold"> {blok.name} </h2>
+      <div className="text-base">
+        {blok.text}
+      </div>
     </div>
   );
 };
